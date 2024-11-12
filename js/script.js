835944bebeb55a3934c1683f9e4ccd907e9fde9b
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-12 16:38:07 +0530

    wip(signup): get signup form details

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const signupForm = document.getElementById("signupForm");
+
+signupForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    
+});
